034d4ef2dae5ffa62a09631a977a3487ca1906c2
Priya Maurya <priya01maurya@gmail.com>
2025-03-14 09:33:03 +0530

    admin dashboard

--- ecommerce-frontend/src/components/AdminSidebar.tsx
+++ ecommerce-frontend/src/components/AdminSidebar.tsx
@@ -1,29 +1,45 @@
-import { IconType } from "react-icons"
-import { Location, Link } from "react-router-dom"
-
+import { RiDatabaseFill, RiShoppingBag3Fill } from "react-icons/ri";
+import { AiFillFileText } from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
+import { Link, useLocation } from "react-router-dom";
 
 const AdminSidebar = () => {
-  return <aside>
-    <h2>Logo.</h2>
-    <div>
-        <h5>
-            Dashboard
-        </h5>
-    </div>
-  </aside>
-}
+  const location = useLocation();
+  return (
+    <aside>
+      <h2>Logo.</h2>
+      <div>
+        <h5>Dashboard</h5>
+        <ul>
+          <li>
+            <Link to={"/admin/dashboard"}>
+              <RiDatabaseFill />
+              Dashborad
+            </Link>
+          </li>
+          <li>
+            <Link to={"/admin/product"}>       
+              <RiShoppingBag3Fill />
+              Product
+            </Link>
+          </li>
+          <li>
+            <Link to={"/admin/customer"}>       
+              <IoIosPeople />
+              Customer
+            </Link>
+          </li>
+          <li>
+            <Link to={"/admin/transaction"}>       
+              <AiFillFileText />
+              Transaction
+            </Link>
+          </li>
+        </ul>
+        {/* <Li url="/admin/dashboard" text="Dashborad" Icon={RiDatabaseFill} location={location}/> */}
+      </div>
+    </aside>
+  );
+};
 
-interface LiProps {
-    url: string,
-    text: string,
-    location: Location,
-    Icon: IconType
-}
-
-const Li =({url, text, location, Icon}:LiProps)=> <li>
-    <Link to={url}>
-    <Icon/>
-    {text}
-    </Link>
-</li>
-export default AdminSidebar
+export default AdminSidebar;
